18d70c1e853d784a03e425d34c542e75c73b1978
LZDXN <47139542+LZDXN@users.noreply.github.com>
2023-10-14 21:38:58 -0500

    modified the git ignore and delete the DS_Store

---
+++ wenquxing/build/precache-manifest.2d7afa53483b886d588dfb3b318d9712.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7a5ac0cf3b0a45f649948af8624ab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4818b0142da318572bfd",
+    "url": "/static/css/main.a5a97279.chunk.css"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "4818b0142da318572bfd",
+    "url": "/static/js/main.10adbcf6.chunk.js"
+  },
+  {
+    "revision": "1209b5989bd700f9325c",
+    "url": "/static/js/runtime~main.e8da6e7d.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
